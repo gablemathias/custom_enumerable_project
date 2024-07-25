87290c745faa68ec36864fb4fd98f6cc2c54a58c
Gabriel Mathias <gablemathias@icloud.com>
2024-07-25 19:10:29 -0300

    own each enumerable

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -7,5 +7,13 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each 
+    if block_given?
+      for value in self
+        yield(value)
+      end
+
+      return self
+    end
+  end
 end
